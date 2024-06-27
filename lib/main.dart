67ccbec2cd27846d089cab7fead4fcb727d4c190
basitansari333 <abdul.basit@bitverx.io>
2024-06-27 11:17:34 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:bloc_pattern_mvp/src/blocs/internet_bloc/internet_bloc.dart';
+import 'package:bloc_pattern_mvp/src/modules/home/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
+
